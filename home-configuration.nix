3e27be6f4c2b9555bcb396bda3f2a2ddb316899d
Nate Smith <nate@theinternate.com>
2022-10-21 13:21:24 -0400

    Make configuration edits commitable

--- home-configuration.nix
+++ home-configuration.nix
@@ -1,6 +1,21 @@
 {
   xdg.enable = true;
-  programs.git.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Nate Smith";
+    userEmail = "nate@theinternate.com";
+  };
+
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+  };
+
+  programs.gh = {
+    enable = true;
+    enableGitCredentialHelper = true;
+  };
 
   home.stateVersion = "22.11";
 }
